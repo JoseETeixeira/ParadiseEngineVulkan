c92bd26e1bc4fa4d7b515c2e897fd1c665a27586
José Eduardo <joseeduardot@gmail.com>
2021-08-16 11:38:27 -0300

    building lua scripts

--- entry/editor/main.cpp
+++ entry/editor/main.cpp
@@ -33,6 +33,11 @@
 
 #include "../../engine/ecs/systems/MeshSystem.hpp"
 
+#include "../third_party/luajit/src/lua.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+
 
 
 Coordinator gCoordinator;
@@ -118,6 +123,73 @@ public:
 		//TODO(eduardo) init all meshSystems 
 		meshSystem->Init(this,getAssetPath() + "models/gltf/FlightHelmet/glTF/FlightHelmet.gltf");
 
+		 int status, result, i;
+    double sum;
+    lua_State *L;
+
+    /*
+     * All Lua contexts are held in this structure. We work with it almost
+     * all the time.
+     */
+    L = luaL_newstate();
+
+    luaL_openlibs(L); /* Load Lua libraries */
+
+    /* Load the file containing the script we are going to run */
+    status = luaL_loadfile(L, "script.lua");
+    if (status) {
+        /* If something went wrong, error message is at the top of */
+        /* the stack */
+        fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(L, -1));
+        exit(1);
+    }
+
+    /*
+     * Ok, now here we go: We pass data to the lua script on the stack.
+     * That is, we first have to prepare Lua's virtual stack the way we
+     * want the script to receive it, then ask Lua to run it.
+     */
+    lua_newtable(L);    /* We will pass a table */
+
+    /*
+     * To put values into the table, we first push the index, then the
+     * value, and then call lua_rawset() with the index of the table in the
+     * stack. Let's see why it's -3: In Lua, the value -1 always refers to
+     * the top of the stack. When you create the table with lua_newtable(),
+     * the table gets pushed into the top of the stack. When you push the
+     * index and then the cell value, the stack looks like:
+     *
+     * <- [stack bottom] -- table, index, value [top]
+     *
+     * So the -1 will refer to the cell value, thus -3 is used to refer to
+     * the table itself. Note that lua_rawset() pops the two last elements
+     * of the stack, so that after it has been called, the table is at the
+     * top of the stack.
+     */
+    for (i = 1; i <= 5; i++) {
+        lua_pushnumber(L, i);   /* Push the table index */
+        lua_pushnumber(L, i*2); /* Push the cell value */
+        lua_rawset(L, -3);      /* Stores the pair in the table */
+    }
+
+    /* By what name is the script going to reference our table? */
+    lua_setglobal(L, "foo");
+
+    /* Ask Lua to run our little script */
+    result = lua_pcall(L, 0, LUA_MULTRET, 0);
+    if (result) {
+        fprintf(stderr, "Failed to run script: %s\n", lua_tostring(L, -1));
+        exit(1);
+    }
+
+    /* Get the returned value at the top of the stack (index -1) */
+    sum = lua_tonumber(L, -1);
+
+    printf("Script returned: %.0f\n", sum);
+
+    lua_pop(L, 1);  /* Take the returned value out of the stack */
+    lua_close(L);   /* Cya, Lua */
+
 		prepared = true;
 	}
 
@@ -158,7 +230,7 @@ public:
 // OS specific macros for the example main entry points
 #if defined(_WIN32)
 // Windows entry point
-#define VULKAN_EXAMPLE_MAIN()																		\
+																	\
 VulkanExample *vulkanExample;																		\
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)						\
 {																									\
@@ -181,7 +253,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)									\
 }
 #elif defined(VK_USE_PLATFORM_ANDROID_KHR)
 // Android entry point
-#define VULKAN_EXAMPLE_MAIN()																		\
+																	\
 VulkanExample *vulkanExample;																		\
 void android_main(android_app* state)																\
 {																									\
@@ -196,7 +268,7 @@ void android_main(android_app* state)																\
 }
 #elif defined(_DIRECT2DISPLAY)
 // Linux entry point with direct to display wsi
-#define VULKAN_EXAMPLE_MAIN()																		\
+																		\
 VulkanExample *vulkanExample;																		\
 static void handleEvent()                                											\
 {																									\
@@ -212,7 +284,7 @@ int main(const int argc, const char *argv[])													    \
 	return 0;																						\
 }
 #elif defined(VK_USE_PLATFORM_DIRECTFB_EXT)
-#define VULKAN_EXAMPLE_MAIN()																		\
+																	\
 VulkanExample *vulkanExample;																		\
 static void handleEvent(const DFBWindowEvent *event)												\
 {																									\
@@ -233,7 +305,7 @@ int main(const int argc, const char *argv[])													    \
 	return 0;																						\
 }
 #elif (defined(VK_USE_PLATFORM_WAYLAND_KHR) || defined(VK_USE_PLATFORM_HEADLESS_EXT))
-#define VULKAN_EXAMPLE_MAIN()																		\
+																	\
 VulkanExample *vulkanExample;																		\
 int main(const int argc, const char *argv[])													    \
 {																									\
@@ -269,7 +341,7 @@ int main(const int argc, const char *argv[])													    \
 }
 #elif (defined(VK_USE_PLATFORM_IOS_MVK) || defined(VK_USE_PLATFORM_MACOS_MVK))
 #if defined(VK_EXAMPLE_XCODE_GENERATED)
-#define VULKAN_EXAMPLE_MAIN()																		\
+																	\
 VulkanExample *vulkanExample;																		\
 int main(const int argc, const char *argv[])														\
 {																									\
@@ -285,8 +357,6 @@ int main(const int argc, const char *argv[])														\
 	}																								\
 	return 0;																						\
 }
-#else
-#define VULKAN_EXAMPLE_MAIN()
 #endif
 #endif
 
