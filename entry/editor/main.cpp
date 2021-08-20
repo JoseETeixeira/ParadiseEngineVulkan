74593d38bb589759133fb1d62afd926858a499d9
José Eduardo <joseeduardot@gmail.com>
2021-08-20 18:33:09 -0300

    cleanup

--- entry/editor/main.cpp
+++ entry/editor/main.cpp
@@ -1,21 +1,15 @@
 /*
-* Vulkan Example - glTF scene loading and rendering
+* Paradise Engine
 *
-* Copyright (C) 2020 by Sascha Willems - www.saschawillems.de
+* Copyright (C) 2021 by José Eduardo da Silva Teixeira Junior - https://developmentparadise.com
 *
 * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
 */
 
+
 /*
- * Shows how to load and display a simple scene from a glTF file
- * Note that this isn't a complete glTF loader and only basic functions are shown here
- * This means no complex materials, no animations, no skins, etc.
- * For details on how glTF 2.0 works, see the official spec at https://github.com/KhronosGroup/glTF/tree/master/specification/2.0
- *
- * Other samples will load models using a dedicated model loader with more features (see base/VulkanglTFModel.hpp)
- *
- * If you are looking for a complete glTF implementation, check out https://github.com/SaschaWillems/Vulkan-glTF-PBR/
- */
+	Entry point for the Paradise Engine
+*/
 
 
 
@@ -127,76 +121,8 @@ public:
 		
 		VulkanExampleBase::prepare();
 
-		//TODO(eduardo) init all meshSystems 
+		//TODO(eduardo) Load everything conditionally from Lua
 		meshSystem->Init(this,getAssetPath() + "models/gltf/FlightHelmet/glTF/FlightHelmet.gltf");
-
-		 int status, result, i;
-    double sum;
-    lua_State *L;
-
-    /*
-     * All Lua contexts are held in this structure. We work with it almost
-     * all the time.
-     */
-    L = luaL_newstate();
-
-    luaL_openlibs(L); /* Load Lua libraries */
-
-    /* Load the file containing the script we are going to run */
-    status = luaL_loadfile(L, "script.lua");
-    if (status) {
-        /* If something went wrong, error message is at the top of */
-        /* the stack */
-        fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(L, -1));
-        exit(1);
-    }
-
-	
-    /*
-     * Ok, now here we go: We pass data to the lua script on the stack.
-     * That is, we first have to prepare Lua's virtual stack the way we
-     * want the script to receive it, then ask Lua to run it.
-     */
-    lua_newtable(L);    /* We will pass a table */
-
-    /*
-     * To put values into the table, we first push the index, then the
-     * value, and then call lua_rawset() with the index of the table in the
-     * stack. Let's see why it's -3: In Lua, the value -1 always refers to
-     * the top of the stack. When you create the table with lua_newtable(),
-     * the table gets pushed into the top of the stack. When you push the
-     * index and then the cell value, the stack looks like:
-     *
-     * <- [stack bottom] -- table, index, value [top]
-     *
-     * So the -1 will refer to the cell value, thus -3 is used to refer to
-     * the table itself. Note that lua_rawset() pops the two last elements
-     * of the stack, so that after it has been called, the table is at the
-     * top of the stack.
-     */
-    for (i = 1; i <= 5; i++) {
-        lua_pushnumber(L, i);   /* Push the table index */
-        lua_pushnumber(L, i*2); /* Push the cell value */
-        lua_rawset(L, -3);      /* Stores the pair in the table */
-    }
-
-    /* By what name is the script going to reference our table? */
-    lua_setglobal(L, "foo");
-
-    /* Ask Lua to run our little script */
-    result = lua_pcall(L, 0, LUA_MULTRET, 0);
-    if (result) {
-        fprintf(stderr, "Failed to run script: %s\n", lua_tostring(L, -1));
-        exit(1);
-    }
-
-    /* Get the returned value at the top of the stack (index -1) */
-    sum = lua_tonumber(L, -1);
-
-    printf("Script returned: %.0f\n", sum);
-
-    lua_pop(L, 1);  /* Take the returned value out of the stack */
-    lua_close(L);   /* Cya, Lua */
 
 		prepared = true;
 	}
