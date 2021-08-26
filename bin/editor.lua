48917b861686c1594481c85c204f4955d309bceb
José Eduardo <joseeduardot@gmail.com>
2021-08-26 16:08:21 -0300

    render multiple meshes

--- bin/editor.lua
+++ bin/editor.lua
@@ -1,9 +1,15 @@
 -- editor.lua
 world = {
-  meshes = {
+  entities = {
     FlightHelmet = {
       path = "../data/models/gltf/FlightHelmet/glTF/FlightHelmet.gltf",
-      position = {px = 0, py = 0, pz = 0},
+      position = {px = 0, py = 0, pz = 1},
+      rotation = {rx = 0, ry = 180, rz = 0},
+      scale = {sx = 1, sy = 1, sz = 1}
+    },
+    FlightHelmet2 = {
+      path = "../data/models/gltf/FlightHelmet/glTF/FlightHelmet.gltf",
+      position = {px = 1, py = 0, pz = 1},
       rotation = {rx = 0, ry = 180, rz = 0},
       scale = {sx = 1, sy = 1, sz = 1}
     }
